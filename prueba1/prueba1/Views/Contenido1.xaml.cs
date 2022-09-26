c0b0252f231867412ba4c35bc42b4858f97bbdac
Julian Mauricio Agudelo Sánchez <julian.agudelo56@correo.tdea.edu.co>
2022-09-26 17:48:12 -0500

    Agregar archivos de proyecto.

---
+++ prueba1/prueba1/Views/Contenido1.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace prueba1.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Contenido1 : ContentPage
+    {
+        public Contenido1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnVolverInicio_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PopAsync();
+        }
+
+        
+    }
+}
---
+++ prueba1/prueba1/Views/practica1.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace prueba1.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class practica1 : ContentPage
+    {
+        public practica1()
+        {
+            InitializeComponent();
+        }
+
+        public async void btnIniciar_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Contenido1());
+        }
+    }
+}
